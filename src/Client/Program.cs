7729b00775f1cff2916526330c6878a94d540d43
Zagidin Selimov <zselimov@adeptik.com>
2024-07-28 21:59:23 +0300

    added notifications

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,10 +2,13 @@ using Client;
 using Client.Auth;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRadzenComponents();
 
 var app = await builder.BuildWithAuthorizationAsync();
 
