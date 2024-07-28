7d809d48d993eefd92c7a9b656acb5e7ab157a4a
Zagidin Selimov <zselimov@adeptik.com>
2024-07-28 21:18:53 +0300

    added validation

--- src/Contracts/Roles/RoleBaseDto.cs
+++ src/Contracts/Roles/RoleBaseDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Contracts.Roles;
 
 /// <summary>
@@ -8,10 +10,13 @@ public class RoleBaseDto
     /// <summary>
     /// Идентификатор.
     /// </summary>
+    [Required(ErrorMessage = "Не указан идентификатор.")]
     public Guid Id { get; set; }
     
     /// <summary>
     /// Название.
     /// </summary>
+    [Required(ErrorMessage = "Не указано название.")]
+    [MinLength(2, ErrorMessage = "Название должно содержать не менее 2 символов.")]
     public string Name { get; set; } = string.Empty;
 }
